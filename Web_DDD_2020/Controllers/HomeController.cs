45f87081567cafb617d35e25555cebf9284fe2a5
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-04-22 21:51:47 -0300

    mudança de nome

--- Web_DDD_2020/Controllers/HomeController.cs
+++ Web_DDD_2020/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace ProjetoDDD.Sensores.Presentation.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
